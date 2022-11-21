98646fcabbff08ac3eeef47df5d86b79a9bdcf11
Lucas Gabriel Eschechola da Silva <lucas.eschechola@outlook.com>
2022-11-21 20:54:36 -0300

    feature: add about page style

--- src/pages/about/about.page.tsx
+++ src/pages/about/about.page.tsx
@@ -1,13 +1,29 @@
-import { Link } from 'react-router-dom';
 import './about.page.css'
 
 
 const AboutPage = () => {
     return(
-        <section>
-            <p>About Page</p>
-            <br />
-            <Link to='/'>Back</Link>
+        <section className='about-page'>
+            <div className='about-first-block'>
+                <div className='about-first-block-item'>
+                    <h2>
+                        Nós construímos <span className='about-first-block-highlight'>pontes </span>
+                        entre <span className='about-first-block-highlight'>petshops e seus clientes</span>
+                    </h2>
+                </div>
+
+                <div className='about-first-block-item'>
+                    <p>
+                        &nbsp;&nbsp;Uma plataforma para entregar conforto e praticidade aos donos de pet
+                        e gestão eficiente para os petshops e estabelecimentos especializados
+                        em saúde e estética animal.
+                    </p>
+                </div>
+            </div>
+
+            <div className='about-second-block'>
+                <img src='images/pimbd-about-background.png' alt='illustrative-background'/>
+            </div>
         </section>
     )
 }
