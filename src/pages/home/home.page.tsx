44004e2deb64d56253bb6ee90151efc8745e945f
Lucas Gabriel Eschechola da Silva <lucas.eschechola@outlook.com>
2022-11-21 21:10:38 -0300

    mend

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,52 +2,70 @@ import './home.page.css'
 
 
 const HomePage = () => {
-    return(
+    return (
         <section>
+            <div className='home-first-block'>
+                <div className='home-first-block-item'>
+                    <h2>
+                        Nutrition has the power to transform pet's lives
+                    </h2>
+                    <p> 
+                        Lorem ipsum dolor sit amet consectetur adipisicing els
+                        praesentium alias dolorem distinctio deleniti repellat, labore incidunt libero?
+                    </p>
+                </div>
+                <div className='home-first-block-item'>
+                    <img src='images/home-background.png' className='img-principal' alt="Principal" />
+                </div>
+            </div>
 
-        <div className="title">
-            <h2 className="title">
-                 Nutrition has the power to transform pet's lives 
-            </h2>
-        </div>
+            <div className='home-second-block'>
+                
+            </div>
 
-        <div className="SUB-TITLE">
+            {/* <div className="title">
+                <h2 className="title">
+                    Nutrition has the power to transform pet's lives
+                </h2>
+            </div>
+
+            <div className="SUB-TITLE">
 
                 <p className="subTitle"> Lorem ipsum dolor sit amet consectetur adipisicing el
-                s praesentium alias dolorem distinctio deleniti repellat, labore incidunt libero?
+                    s praesentium alias dolorem distinctio deleniti repellat, labore incidunt libero?
                 </p>
-        </div>
-        
-        <div className="holder">    
-          <div className="imagem" data-title="">
+            </div>
 
-          <img src='images/home-background.png' className='img-principal' alt="Principal" />
-    
-          </div>
-        </div>
+            <div className="holder">
+                <div className="imagem" data-title="">
 
-        <div className="btn">
+                    <img src='images/home-background.png' className='img-principal' alt="Principal" />
 
-         <form>
+                </div>
+            </div>
 
-            <button type="submit" className='btn-principal'> Get Started </button>
-        
-        </form>
-        </div>
+            <div className="btn">
 
-        <div className="btn-2">
+                <form>
 
-            <form>
+                    <button type="submit" className='btn-principal'> Get Started </button>
 
-            <button type='submit' className='btn-transp' > Learn More </button>
+                </form>
+            </div>
 
-            </form>
-        </div>
-     
+            <div className="btn-2">
+
+                <form>
+
+                    <button type='submit' className='btn-transp' > Learn More </button>
+
+                </form>
+            </div> */}
+
         </section>
 
-        
+
     )
 }
 
- export default HomePage;
+export default HomePage;
