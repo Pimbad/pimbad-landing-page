597d9a91d689830a85f4994c52965395feb5d257
Lucas Eschechola <37378400+Eschechola@users.noreply.github.com>
2022-11-22 02:16:40 -0300

    feature: add reviews page (#6)

--- src/pages/reviews/reviews.page.tsx
+++ src/pages/reviews/reviews.page.tsx
@@ -1,15 +1,144 @@
-import { Link } from 'react-router-dom';
 import './reviews.page.css'
 
 
 const ReviewsPage = () => {
     return(
         <section>
-            <p>Review Page</p>
-            <br />
-            <Link to='/'>Back</Link>
+            <div className='review-first-block'>
+                <div className='review-first-block-content'>
+                    <h2>O que nossos usuários dizem?</h2>
+                    
+                    <div className='review-first-block-itens'>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer' />
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                    </div>
+
+                    <div className='review-first-block-itens'>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                    </div>
+
+                    <div className='review-first-block-itens'>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                        <div className='review-first-block-item'>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore 
+                            </p>
+
+                            <img src='images/review-image.jpg' alt='reviewer'/>
+                            <h5>Jalim Habei</h5>
+                            <h6>CEO Petlovers</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
-        
     )
 }
 
